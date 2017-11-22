2f0764db0ea24e3940ba901b79a00fa5754b84e0
Aleksei-B <aleksei-b@protonmail.com>
2017-11-22 21:08:32 +0700

    Implement list sharing between users

--- functional_tests/test_sharing.py
+++ functional_tests/test_sharing.py
@@ -26,13 +26,13 @@ class SharingTest(FunctionalTest):
         # Edith goes to the home page and starts a list
         self.browser = edith_browser
         self.browser.get(self.live_server_url)
-        list_page = ListPage(self).add_list_item('Get Help')
+        list_page = ListPage(self).add_list_item('Get help')
         
         # She notices a "Share this list" option
         share_box = list_page.get_share_box()
         self.assertEqual(
             share_box.get_attribute('placeholder'),
-            'your-friend@example.com'
+            'your@friends-email.com'
         )
         
         # She shares her list.
--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -90,3 +90,8 @@ class ListModelTest(TestCase):
         Item.objects.create(list=list_, text='second item')
         self.assertEqual(list_.name, 'first item')
         
+    def test_shared_with_dot_add_saves_users(self):
+        user = User.objects.create(email='fred@example.com')
+        list_ = List.objects.create()
+        list_.shared_with.add('fred@example.com')
+        self.assertIn(user, list_.shared_with.all())
